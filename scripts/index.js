6a16eb9809b91382cd1277d17efafa30777f20da
BrCasagrande <brunopcasagrande@gmail.com>
2018-04-24 18:22:20 -0300

    Add nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,5 @@
 "use strict";
 /// <reference path ="../node_modules/@types/jquery/index.d.ts"/> 
 var p = document.createElement("p");
-p.textContent = "Ola Mundo";
 document.body.appendChild(p);
-$(document).ready(function () {
-    $("p").click(function () {
-        $(this).hide();
-    });
-});
 //# sourceMappingURL=index.js.map
---
+++ scripts/nave.js
@@ -0,0 +1,7 @@
+"use strict";
+$(document).ready(function () {
+    $("p").click(function () {
+        $(this).hide();
+    });
+});
+//# sourceMappingURL=nave.js.map
