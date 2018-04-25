cf195a3a40b0bde2cab3a3f0083a45c2c987c191
BrCasagrande <brunopcasagrande@gmail.com>
2018-04-25 19:06:09 -0300

    Arrumando o menu

--- scripts/nave.ts
+++ scripts/nave.ts
@@ -1,16 +1,24 @@
 
+const init = () =>{
+    createMenu();
+    addItens();
+}
 
-let menuPrincipal = $('nav-principal');
 
+const item = [ {"nome":"Home", "url":"#"},{"nome":"Simulador", "url":"#"}
+,{"nome":"Produtos", "url":"#","itens":["1","vr"]}];
+
+const createMenu = () =>{
+    let menuPrincipal = $('nav-principal');    
 let nav = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuPrincipal" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                     
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="menuPrincipal">
+                    <ul id ="menu-itens" class="navbar-nav mr-auto">
                         <li class="nav-item active">
                             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
@@ -19,10 +27,10 @@ let nav = `
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Produtos</a>
-                            <ul class="navbar-nav d-none">
+                            <ul class="navbar-nav bg-dark menuShow">
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">Painéis Solares</a>
-                                    <ul class="navbar-nav">
+                                    <ul class="navbar-nav bg-dark menuShow">
                                         <li class="nav-item"><a href="#" class="nav-link">Até 95w</a></li>
                                         <li class="nav-item"><a href="#" class="nav-link">100w até 195w</a></li>
                                         <li class="nav-item"><a href="#" class="nav-link">200w até 295w</a></li>
@@ -38,5 +46,28 @@ let nav = `
                     </form>
                 </div>
             </nav>   `;
+    menuPrincipal.append(nav);
+}
 
-menuPrincipal.append(nav);
+const addItens = () =>{    
+    item.forEach(n => {
+    let it = `<li class="nav-item">
+    <a class="nav-link" href="${n.url}">${n.nome}</a>        
+    </li>`;
+    $("#menu-itens").append(it);
+    if(n.itens){
+        let sub = `<ul class="navbar-nav bg-dark menuShow"></ul>`;
+        
+        n.itens.forEach(m =>{
+            let subItem = `<li class="nav-item">
+            <a href="#" class="nav-link">${m}</a></li>`;            
+        });
+        console.log(n);
+    }
+    
+    });
+}
+
+init();
+$("li").hover(function(){$(this).children('ul').css("display", "block")}
+, function(){$(this).children('ul').css("display", "none")});
