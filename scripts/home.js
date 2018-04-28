c6af17016a62ed97cf74ee8a5f62ca184a523fac
BrCasagrande <brunopcasagrande@gmail.com>
2018-04-28 20:52:28 -0300

    auto item++ carousel

---
+++ scripts/home.js
@@ -0,0 +1,13 @@
+"use strict";
+var buildCarousel = function () {
+    var carousel = [
+        { "img": "img/1.png", "alt": "Casa Tesla" },
+        { "img": "img/2.png", "alt": "Painel Solar" },
+        { "img": "img/3.png", "alt": "Sistema Solar" },
+        { "img": "img/4.png", "alt": "Bateria Tesla" }
+    ];
+    for (var item in carousel) {
+        console.log(item);
+    }
+};
+//# sourceMappingURL=home.js.map
